95caedeb208a8f5cba1bb171235c4ca3d931a6d8
Ali Moallim <amoallim15@gmail.com>
2024-11-14 12:19:08 +0300

    tmp fix

--- src/app/(locale)/[locale]/not-found.tsx
+++ src/app/(locale)/[locale]/not-found.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import E404View from "@/presentation/errors/404/view";
 
-export default function NotFound() {
+export default function Page() {
   return <E404View />;
 }
