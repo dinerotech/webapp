d291c924656ff4ccfdc7347f37385ba5e542fd54
Ali Moallim <amoallim15@gmail.com>
2024-11-14 05:55:03 +0300

    Update view.tsx

--- src/presentation/privacyPolicy/contents/view.tsx
+++ src/presentation/privacyPolicy/contents/view.tsx
@@ -119,6 +119,8 @@ export default function View() {
             <figcaption className="mt-6 flex gap-x-4">
               <Image
                 className="size-6 flex-none rounded-full bg-gray-50"
+                width={256}
+                height={256}
                 src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                 alt=""
               />
@@ -141,6 +143,8 @@ export default function View() {
           <Image
             className="aspect-video rounded-xl bg-gray-50 object-cover"
             src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3"
+            width={1310}
+            height={873}
             alt=""
           />
           <figcaption className="mt-4 flex gap-x-2 text-sm/6 text-gray-500">
